aef7d40aaa2602b02506c2bd5da58ccb7ec68c97
Agnese Kapnere <akapne01@gmail.com>
2019-07-17 21:08:54 +0100

    Add comments for ch 2 abstract classes

---
+++ src/OCA_Programmer_Exam_Guide/Ch_2_Object_orientation/Ball.java
@@ -0,0 +1,13 @@
+package OCA_Programmer_Exam_Guide.Ch_2_Object_orientation;
+
+import OCA_Programmer_Exam_Guide.Ch1_Declarations_and_Access_Control.Bounceable;
+
+abstract public class Ball implements Bounceable {
+    /**
+     * It is legal to implement methods with no method body.
+     * In case methods have a return type, then body needs
+     * to have a return statement to compile.
+     */
+//    public void bounce() {}
+//    public void setBounceFactor(int bf) {}
+}
---
+++ src/OCA_Programmer_Exam_Guide/Ch_2_Object_orientation/BeachBall.java
@@ -0,0 +1,25 @@
+package OCA_Programmer_Exam_Guide.Ch_2_Object_orientation;
+// p. 123
+public class BeachBall extends Ball {
+    @Override
+    public void bounce() {
+        // Interesting BeachBall specific Bounce code
+
+    }
+
+    @Override
+    public void setBounceFactor(int bf) {
+        // Clever beachball specific code for setting a bounce factor.
+
+    }
+    /**
+     * Even though we don't say in the class declaration above,
+     * BeachBall implements Bounceable, since beachball's abstract
+     * superclass ball implements boouncable.
+     *
+     * If class Ball would have any abstract methods, they would
+     * have to be implemented here as well.
+     */
+
+
+}
