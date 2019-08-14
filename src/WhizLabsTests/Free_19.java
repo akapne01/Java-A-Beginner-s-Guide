7fc7ceb185b18fac3b133af5b19aef833887a7ab
Agnese Kapnere <agnese.kapnere@ig.com>
2019-08-14 17:54:34 +0100

    Adding some exercises

--- src/WhizLabsTests/Free_19.java
+++ src/WhizLabsTests/Free_19.java
@@ -1,7 +1,16 @@
 package WhizLabsTests;
 
+import java.util.Arrays;
+
 public class Free_19 {
+
+   static Integer i;
+
+
    public static void main(String[] args) {
+
+      final int kkk;
+      kkk = 9;
       StringBuilder sb = new StringBuilder("1Z0");
       /**
        * Below line does not compile, because
@@ -9,5 +18,16 @@ public class Free_19 {
        */
 //      sb.concat("-808");
       System.out.println(sb);
+      int []a = {0123};
+      int []b = new int[0b101];
+
+      System.out.println(i);
+
+      /**
+       * ClassCastException is thrown as arrays cannot be compared to strings.
+       */
+      String[] d2[] = {{"c", "d"}, {"a", "b"}};
+//      Arrays.sort(d2);
+//      System.out.println(Arrays.toString(d2));
    }
 }
